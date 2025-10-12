ef589782f3564c3048be87a9c38427d6acfc232d
admin <khoahocmaytinh37@gmail.com>
2025-10-12 23:28:19 +0700

    add slider

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,14 +1,19 @@
-import { useTranslation } from "react-i18next";
 import "./Home.css";
-import TopSearch from "./topSearch/TopSearch";
 import Products from "./products/Products";
+import Slider from "./slider/Slider";
+import images from "./slider/image";
+import TopSearch from "./topSearch/TopSearch";
 
 export default function Home() {
-    const { t } = useTranslation();
-    return (
-        <div className="main-content">
-            <TopSearch />
-            <Products />
-        </div>
-    );
+  return (
+    <div className="main-content">
+      <Slider>
+        {images.map((image, index) => {
+          return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+        })}
+      </Slider>
+      <TopSearch />
+      <Products />
+    </div>
+  );
 }
---
+++ frontend/src/pages/home/slider/Slider.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState, type ReactNode } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import "./Slider.css";
+
+interface SliderProps {
+  children: ReactNode[];
+}
+
+export default function Slider({ children }: SliderProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [slideDone, setSlideDone] = useState(true);
+  const [timeID, setTimeID] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (slideDone) {
+      setSlideDone(false);
+      setTimeID(
+        setTimeout(() => {
+          slideNext();
+          setSlideDone(true);
+        }, 3000)
+      );
+    }
+  }, [slideDone]);
+
+  const slideNext = () => {
+    setActiveIndex((val) => {
+      if (val >= children.length - 1) {
+        return 0;
+      } else {
+        return val + 1;
+      }
+    });
+  };
+
+  const slidePrev = () => {
+    setActiveIndex((val) => {
+      if (val <= 0) {
+        return children.length - 1;
+      } else {
+        return val - 1;
+      }
+    });
+  };
+
+  const AutoPlayStop = () => {
+    if (timeID !== null) {
+      clearTimeout(timeID);
+      setSlideDone(false);
+    }
+  };
+
+  const AutoPlayStart = () => {
+    if (!slideDone) {
+      setSlideDone(true);
+    }
+  };
+
+  return (
+    <div className="slider">
+      <div
+        className="container__slider"
+        onMouseEnter={AutoPlayStop}
+        onMouseLeave={AutoPlayStart}
+      >
+        {children.map((item, index) => (
+          <div
+            className={"slider__item slider__item-active-" + (activeIndex + 1)}
+            key={index}
+          >
+            {item}
+          </div>
+        ))}
+
+        <button
+          className="slider__btn-prev"
+          onClick={(e) => {
+            e.preventDefault();
+            slidePrev();
+          }}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="slider__btn-next"
+          onClick={(e) => {
+            e.preventDefault();
+            slideNext();
+          }}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      <div className="container__slider__links">
+        {children.map((item, index) => (
+          <button
+            key={index}
+            className={
+              activeIndex === index
+                ? "container__slider__links-small container__slider__links-small-active"
+                : "container__slider__links-small"
+            }
+            onClick={(e) => {
+              e.preventDefault();
+              setActiveIndex(index);
+            }}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
